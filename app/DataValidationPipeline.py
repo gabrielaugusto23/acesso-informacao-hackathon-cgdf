7a6b8bdc0f2ddd084aac055fa23dde6ae0e2267d
Joao Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2026-01-30 18:45:38 -0300

    adiciona email

--- app/DataValidationPipeline.py
+++ app/DataValidationPipeline.py
@@ -1,4 +1,5 @@
 from validators.CPFValidator import CPFValidator
+from validators.EmailValidator import EmailValidator
 from validators.NameValidator import NameValidator
 from validators.PhoneValidator import PhoneValidator
 from validators.RGValidator import RGValidator
@@ -7,4 +8,6 @@ from validators.RGValidator import RGValidator
 class DataValidationPipeline:
     @staticmethod
     def build():
-        return PhoneValidator(RGValidator(CPFValidator(NameValidator(None))))
+        return PhoneValidator(
+            RGValidator(CPFValidator(EmailValidator(NameValidator(None))))
+        )
