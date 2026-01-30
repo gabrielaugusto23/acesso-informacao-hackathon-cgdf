b1dc40451c1a3a1ee4286ad3acab167d0b3a4b20
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2026-01-30 18:52:45 -0300

    refatora CPFValidator para melhorar a validação de CPF

--- app/validators/CPFValidator.py
+++ app/validators/CPFValidator.py
@@ -1,25 +1,23 @@
 import re
-
+from validate_docbr import CPF
 from app.validators.IValidator import IValidator
 from app.validators.ValidationResult import ValidationResult
 
-
 class CPFValidator(IValidator):
-
     def __init__(self, next_handler=None):
         super().__init__(next_handler)
+        self.cpf_validator = CPF()
 
-    def handle(self, text: str) -> str:
-
+    def handle(self, text: str):
         cpf_pattern = r"\b\d{3}\.?\d{3}\.?\d{3}-?\d{2}\b"
+        matches = re.findall(cpf_pattern, text)
 
-        if re.search(cpf_pattern, text):
-            text = re.sub(cpf_pattern, "[CPF_REDACTED]", text)
-
-            return ValidationResult(
-                detected_value="VALOR CPF",
-                validators="CPFValidator",
-            )
+        for match in matches:
+            if self.cpf_validator.validate(match):
+                return ValidationResult(
+                    detected_value=f"CPF: {match}",
+                    validators="CPFValidator",
+                )
 
         if self._next_handler is not None:
             return self._next_handler.handle(text)
